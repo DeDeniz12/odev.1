94740519f2e83eec19d8a1a3b89ee5108fd4710b
DeDeniz12 <ulucaglayandeniz@gmail.com>
2026-01-17 21:53:20 +0300

    main

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,67 +1,20 @@
 package frc.robot;
 
-
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.FeederSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.feederSubsystem;
+import frc.robot.subsystems.hoodSubsystem;
+import frc.robot.subsystems.intakeSubsystem;
 
 public class RobotContainer {
+    public static final intakeSubsystem intake = new intakeSubsystem();
+  public static final feederSubsystem feeder = new feederSubsystem();
+  public static final hoodSubsystem hood = new hoodSubsystem();
   
-  // Alt sistemler
-  private final IntakeSubsystem m_intake = new IntakeSubsystem();
-  private final FeederSubsystem m_feeder = new FeederSubsystem();
-
-  // Joystick Tanımlama
-  private final Joystick m_driverController = new Joystick(0);
-
   public RobotContainer() {
-    configureBindings();
-  }
 
-  private void configureBindings() {
-    
-    //TUŞ 1 GÖREVİ
-    
-    new JoystickButton(m_driverController, 1)
-        .whileTrue( // Tuşa basılı tutulduğu sürece
-            Commands.run( // 1. Motorlar
-                () -> {
-                  m_intake.setVoltage(5.0);
-                  m_feeder.setVoltage(5.0);
-                }, m_intake, m_feeder
-            )
-            // 2. KOMUT
-            .until(() -> m_feeder.isSensorTriggered())
-            
-            // 3. Komut bittiğinde
-            .finallyDo(() -> {
-                m_intake.stop();
-                m_feeder.stop();
-            })
-        );
-
-
-    //TUŞ 2 GÖREVİ
-    
-    new JoystickButton(m_driverController, 2)
-        .whileTrue(
-            Commands.run(
-                () -> {
-                    m_intake.setVoltage(-5.0); // Eksi voltaj yani Geri Yön
-                    m_feeder.setVoltage(-5.0);
-                }, m_intake, m_feeder
-            )
-            .finallyDo(() -> {
-                m_intake.stop();
-                m_feeder.stop();
-            })
-        );
   }
 
   public Command getAutonomousCommand() {
-    return Commands.none();
+    return null;
   }
 }
