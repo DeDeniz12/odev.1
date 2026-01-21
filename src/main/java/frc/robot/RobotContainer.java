bc4b495283bdd628736fa134c95978fdcaf3d0af
DeDeniz12 <ulucaglayandeniz@gmail.com>
2026-01-21 21:29:33 +0300

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,6 @@ import frc.robot.subsystems.intakeSubsystem;
 public class RobotContainer {
     public static final intakeSubsystem intake = new intakeSubsystem();
   public static final feederSubsystem feeder = new feederSubsystem();
-  public static final hoodSubsystem hood = new hoodSubsystem();
   
   public RobotContainer() {
 
