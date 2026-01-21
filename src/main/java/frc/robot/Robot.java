f8b1281655455293f71a1c94b7d248cadc60bbfc
DeDeniz12 <ulucaglayandeniz@gmail.com>
2026-01-21 21:26:58 +0300

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,19 +1,18 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  private RobotContainer m_robotContainer;
 
+  private final RobotContainer m_robotContainer;
 
-  private final Joystick stick_0 = new Joystick(0);
+  PS5Controller driverJoystick = new PS5Controller(0);
 
-  @Override
-  public void robotInit() {
+  public Robot() {
     m_robotContainer = new RobotContainer();
   }
 
@@ -23,49 +22,71 @@ public class Robot extends TimedRobot {
   }
 
   @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
   public void teleopInit() {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
   }
 
-
   @Override
   public void teleopPeriodic() {
-    
 
-    boolean sensorDolu = RobotContainer.feeder.getSensorFeeder();
-
-
-    
-    if (sensorDolu == false) {
-
-      RobotContainer.hood.setHoodAngle(30);
-    } 
-    else {
-
-      
-      if (stick_0.getRawButton(1)) {
-
-        RobotContainer.hood.setHoodAngle(50);
-      } 
-      else if (stick_0.getRawButton(2)) {
-
-        RobotContainer.hood.setHoodAngle(80);
+    if (driverJoystick.getRawButton(1)) {
+      if (RobotContainer.feeder.getFeederSensor()) {
+        RobotContainer.intake.setIntakeVoltage(0);
+        RobotContainer.feeder.setFeederVoltage(0);
+      } else {
+        RobotContainer.intake.setIntakeVoltage(5);
+        RobotContainer.feeder.setFeederVoltage(5);
       }
+    } else if (driverJoystick.getRawButton(2)) {
+      RobotContainer.intake.setIntakeVoltage(-5);
+      RobotContainer.feeder.setFeederVoltage(-5);
+    } else {
+      RobotContainer.intake.setIntakeVoltage(0);
+      RobotContainer.feeder.setFeederVoltage(0);
     }
   }
 
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-  
   @Override
   public void testInit() {
+    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {
+  }
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {
+  }
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {
   }
 }
